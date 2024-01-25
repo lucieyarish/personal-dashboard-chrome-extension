253fbd407330b5cb2ab3f55ee96fb0f9e2a3f165
Lucie Yarish <lucieyarish@hotmail.com>
2024-01-25 07:37:32 +0100

    Set default image and location if data unavailable

--- js/index.js
+++ js/index.js
@@ -5,7 +5,11 @@ fetch(
 )
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
     document.body.style.backgroundImage = `url(${data.urls.full})`;
     locationEl.textContent = data.location.name;
+  })
+  .catch(() => {
+    document.body.style.backgroundImage =
+      "url('/assets/images/default-image.jpg')";
+    locationEl.textContent = `Doi Hua Mae Kham, Mae Salong Nai, Thailand`;
   });
